1e5a6c124938410e7bbdd714de379724739b5833
cass <76407605+cleclair71@users.noreply.github.com>
2023-01-17 16:40:11 -0500

    remove old rabbithole code, add functions

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ var goBack = document.getElementById('goback-btn');
 var currentQuestion = quizQuestions[currentQIndex];
 var timerPreset = 60;
 var timeLeft = timerPreset;
-
+var clearBtn = document.getElementById('clear-btn')
 
 function mainMenu() {
   timeLeft = timerPreset;
@@ -56,16 +56,23 @@ function mainMenu() {
 
 }
 //   // Timer
-function countdown() = setInterval(function () {
-let 
-  countdown--;
-  document.getElementById("timer").innerHTML = countdown;
-  if (countdown <= 0) {
-    clearInterval(intervalId);
-    alert("Time's up!");
-    stopQuiz();
+function startTimer() {
+  var timerInterval = setInterval(function() {
+    if(gameEnd === true) { //test if game ended before anything incase needs to be stopped
+      clearInterval(timerInterval); //stop
+      return;
+    }
+      if(timeLeft < 1) { //if timer is out under 1 cause wrong answers subtract 10 seconds game ends and timer stops
+        clearInterval(timerInterval); //stop 
+        alert("Time's up!");
+        quizEnd ();
+      }
+
+      timer1.textContent = timeLeft; //update timer
+      timeLeft--; 
+    }, 1000); 
+return;
   }
-}, 1000); 
 
 function quizStart() {
 // hide all divs
@@ -118,7 +125,7 @@ function clickQ(event) {
       time = 0;
     }
 
-    timer1.textContent = time;
+    timer1.textContent = timerPreset;
 
     responsePrompt.textContent = 'Wrong Answer!';
   } else {
@@ -130,7 +137,7 @@ function clickQ(event) {
     responsePrompt.setAttribute('class', 'response hide');
   }, 1000);
 }
-
+// goes to next quesitons
 currentQIndex++;
 
 function allDone() {
@@ -141,21 +148,76 @@ var quizEnd = document.getElementById('alldone');
 quizEnd.removeAttribute('class');
 // show score
 var finalScore = document.getElementById('score');
-finalScore.textContent = time;
+finalScore.textContent = timeLeft;
 // hide question divs
 options1.setAttribute('class', 'hide');
 
 }
 
 function saveResults() {
+initialInput = document.querySelector ('input');
+var tempArray = [];
+
+if (initialInput.value != '' || initialInput.value != null) {
+  var tempObject = {
+    initials: initialInput.value,
+    scores: score, 
+  }
+  if(window.localStorage.getItem(`highscores`) == null) { //if no data exists create a new
+    tempArray.push(tempObject); 
+    window.localStorage.setItem('highscores', JSON.stringify(tempArray)); // send to storage
+} else { // if there's data already stored, add new data
+tempArray = JSON.parse(window.localStorage.getItem('highscores'));
+for (let index = 0; index <= tempArray.length; index++) {
+  if (index == tempArray.length) {
+    tempArray.push(tempObject);
+    break; // stop loop
+  } else if (tempArray[index].scores < score) {
+    tempArray.splice(index, 0, tempObject);
+    break;// stop loop
+  }
+  }
+}
+window.localStorage.setItem('highscores', JSON.stringify(tempArray)) 
+} 
+document.querySelector(`input`).value = ``;
+score = 0;
 
 }
 
 function checkEnter(event) {
 
 }
-submit1.onclick = saveResults;
 
+function showHighscores() {
+  scoreList = document.querySelector(`ol`);
+  scoreList.innerHTML = ''
+
+  tempArray = JSON.parse(window.localStorage.getItem(`highscores`));
+  if (tempArray != null) {
+    for (let index = 0 < tempArray.length; index++) {
+      var resultsLi = document.createElement('li')
+      resultsLi.textContent = tempArray[index].initials + ' - ' + tempArray[index].scores;
+      scoreList.appendChild(resultsLi);
+    }
+
+  } else { // if no information is stored
+    var resultsLi = document.createElement(`p`)
+    resultsLi.textContent = 'No Highscores Saved'
+    scoreList.appendChild(resultsLi);
+  }
+  return;
+}
+
+function clearScores() {
+  document.querySelector(`ol`).innerHTML = ``; 
+  window.localStorage.clear();
+  return;
+}
+
+
+submit1.onclick = saveResults;
+clearBtn.onclick = clearScores;
 startBtn.onclick = quizStart;
 options1.onclick = clickQ;
 initials1.onkeyup = checkEnter;
@@ -184,205 +246,3 @@ function displayQuizResults() {
 var currentQuestion = 0;
 let countdown = 60;
 
-function showNextQuestion() {
-  document.getElementById("question" + currentQuestion).style.display = "none";
-  currentQuestion++;
-  document.getElementById("question" + currentQuestion).style.display = "block";
-}
-
-// hide on load
-window.onload = function() {
-  document.getElementById("startingpage").style.display = "block";
-  for (var i = 1; i <= 6; i++) {
-    document.getElementById("question" + i).style.display = "none";
-  }
-};
-
-document.getElementById("start-timer-btn").addEventListener("click", function () {
-  document.getElementById("question2").style.display = "block";
-  // hide divs
-
-document.getElementById("strings-btn").addEventListener("click", function() {
-  // code to check if answer is correct
-  showNextQuestion();
-});
-
-
-});
-// hide divs
-var elements = document.querySelectorAll("#startingpage, #alldone, #question3, #question4, #question5, #question6");
-  elements.forEach(function(element){
-    element.style.display = "none";
-  });
-
-// Start Page
-let startTimerBtn = document.getElementById("start-timer-btn");
-startTimerBtn.addEventListener("click", function () {
-    document.getElementById("question2").style.display = "block";
-  // 
-  // var elements = document.querySelectorAll("#startingpage, #alldone, #question3, #question4, #question5, #question6");
-  // elements.forEach(function(element){
-  //   element.style.display = "none";
-  // });
-  let rightAnswerBtn = document.getElementById("rightAnswerBtn");
-rightAnswerBtn.addEventListener("click", function(){
-    score += 1;
-    highScores.push(score);
-    document.getElementById("question4").style.display = "block";
-    document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question5, #question6").forEach(function(element){
-        element.style.display = "none";
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-//QUESTION 2 - add event listener to right answer
-let rightAnswerBtn = document.getElementById("alerts-btn");
-rightAnswerBtn.addEventListener("click", function(){
-    score += 10;
-    highScores.push(score);
-    showNextQuestion();
-    // document.getElementById("question3").style.display = "block";
-    // document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question4, #question5").forEach(function(element){
-    //     element.style.display = "none";
-    // });
-});
-
-//WRONG ANSWERS DONT WORK
-//add event listener to wrong answers
-let wrongAnswerBtns = document.querySelectorAll("#strings-btn, #booleans-btn, #numbers-btn");
-wrongAnswerBtns.forEach(function(btn){
-    btn.addEventListener("click", function(){
-        countdown -= 10;
-        document.getElementById("question" + currentQuestion).innerHTML = "Wrong Answer";
-    showNextQuestion();
-        
-        });
-    });
-});
-//QUESTION 3 - add event listener to right answer
-//add event listener to wrong answers
-let rightAnswerBtn = document.getElementById("parenthesis-btn");
-rightAnswerBtn.addEventListener("click", function(){
-    score += 1;
-    highScores.push(score);
-    document.getElementById("question4").style.display = "block";
-    document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question5, #question6").forEach(function(element){
-        element.style.display = "none";
-    });
-});
-
-//wrong answers
-//add event listener to wrong answers
-let wrongAnswerBtns = document.querySelectorAll("#quotes-btn, #curly-btn, #square-btn");
-wrongAnswerBtns.forEach(function(btn){
-    btn.addEventListener("click", function(){
-        countdown -= 10;
-        document.getElementById("question" + currentQuestion).innerHTML = "Wrong Answer";
-    showNextQuestion();
-        });
-    });
-
-//QUESTION 4 - add event listener to right answer
- let rightAnswerBtn = document.getElementById("quotes2-btn");
-rightAnswerBtn.addEventListener("click", function(){
-    score += 1;
-    highScores.push(score);
-    document.getElementById("question5").style.display = "block";
-    document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question4, #question6").forEach(function(element){
-        element.style.display = "none";
-    });
-});
-
-//wrong answers
-let wrongAnswerBtns = document.querySelectorAll("#commas-btn, #curly2-btn, #parenthesis2-btn");
-wrongAnswerBtns.forEach(function(btn){
-    btn.addEventListener("click", function(){
-        countdown -= 10;
-        document.getElementById("question5").style.display = "block";
-        document.getElementById("question5").innerHTML = "Wrong Answer";
-        document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question4, #question6").forEach(function(element){
-            element.style.display = "none";
-        });
-    });
-});
- //QUESTION 5 - add event listener to right answer
- let rightAnswerBtn = document.getElementById("console-btn");
- rightAnswerBtn.addEventListener("click", function(){
-     score += 1;
-     highScores.push(score);
-     document.getElementById("question6").style.display = "block";
-     document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question4, #question5").forEach(function(element){
-         element.style.display = "none";
-     });
- });
- 
- //wrong answers
- let wrongAnswerBtns = document.querySelectorAll("#JS-btn, #bash-btn, #loops-btn");
- wrongAnswerBtns.forEach(function(btn){
-     btn.addEventListener("click", function(){
-         countdown -= 10;
-         document.getElementById("question6").style.display = "block";
-         document.getElementById("question6").innerHTML = "Wrong Answer";
-         document.querySelectorAll("#startingpage, #question2, #alldone, #question2, #question3, #question4, #question5").forEach(function(element){
-             element.style.display = "none";
-         });
-     });
- });
- //QUESTION 6 - add event listener to right answer
- let rightAnswerBtn = document.getElementById("above-btn");
- rightAnswerBtn.addEventListener("click", function(){
-     score += 1;
-     highScores.push(score);
-     document.getElementById("alldone").style.display = "block";
-     document.querySelectorAll("#startingpage, #question2, #question6, #question2, #question3, #question4, #question5").forEach(function(element){
-         element.style.display = "none";
-     });
- });
- 
- //wrong answers
- let wrongAnswerBtns = document.querySelectorAll("#n&s-btn, #other-btn, #bool-btn");
- wrongAnswerBtns.forEach(function(btn){
-     btn.addEventListener("click", function(){
-         countdown -= 10;
-         document.getElementById("alldone").style.display = "block";
-         document.getElementById("alldone").innerHTML = "Wrong Answer";
-         document.querySelectorAll("#startingpage, #question2, #question6, #question2, #question3, #question4, #question5").forEach(function(element){
-             element.style.display = "none";
-         });
-     });
- });
- //ALL DONE
- 
- if (currentQuestion === 6) {
-  clearInterval(intervalId);
-  document.getElementById("alldone").style.display = "block";
-  document.getElementById("alldone").innerHTML = "Your final score is:" + score;
-  let initials = input({initials: initials, score: score});
-  storeQuizResults(highScores);
- }
-
-
-// Timer has run down and you are returned to results page
-function stopQuiz() {
-    var x = document.querySelectorAll("#startingpage, #question2, #question3, #question3,#question4, question5");
-    var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById("alldone").style.display = "block";
-  document.getElementById("alldone").innerHTML = "You scored: " + calculateResults();
-  function calculateResults() {
-    return score;
-}
-}
